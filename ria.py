de26bf7f5a315e543e632a35fa09e55e0efb2e5d
Dmitriy <49707091+DmitriyKhodykin@users.noreply.github.com>
2019-10-31 09:56:34 +0300

    Update ria.py

--- ria.py
+++ ria.py
@@ -1,18 +1,20 @@
 # Модуль для разбора новостной ленты "Риа"
+# pip install lxml
 
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
 
-source = requests.get('https://www.ria.ru').text
-soup = BeautifulSoup(source, 'lxml')
+def ria():
+    source = requests.get('https://www.ria.ru').text
+    soup = BeautifulSoup(source, 'lxml')
 
-news_list = []
-for main_line in soup.find_all('span', class_='cell-list__item-desc'):
-    news_main = main_line.span.text.split('/')
-    for item in news_main:
-        news_list.append(item)
+    news_list = []
+    for main_line in soup.find_all('span', class_='cell-list__item-desc'):
+        news_main = main_line.span.text.split('/')
+        for item in news_main:
+            news_list.append(item)
 
-df_ria = pd.DataFrame(news_list)
-print(df_ria)
+    df_ria = pd.DataFrame(news_list)
+    return df_ria
