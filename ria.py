9adac80465cec048a55e26ed6bc824f0af6d392e
Dmitriy <49707091+DmitriyKhodykin@users.noreply.github.com>
2020-02-06 16:18:14 +0300

    Update ria.py

--- ria.py
+++ ria.py
@@ -1,12 +1,14 @@
 # Модуль для разбора новостной ленты "Риа"
 # pip install lxml
 
+# Импорты
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
 
 def ria():
+    """Возвращает заголовки новостей с главной Риа.ру"""
     source = requests.get('https://www.ria.ru').text
     soup = BeautifulSoup(source, 'lxml')
 
