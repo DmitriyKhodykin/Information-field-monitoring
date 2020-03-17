f5cf2faa13305cbece31a7668a2384754e7c09e0
Dmitriy <49707091+DmitriyKhodykin@users.noreply.github.com>
2020-03-17 19:33:14 +0300

    Update tass_parser.py

--- tass_parser.py
+++ tass_parser.py
@@ -1,40 +1,41 @@
-# Модуль для выгрузки новостной ленты ТАСС в локальную базу данных
+# Module for uploading TASS news feed to a local database
 # Python 3.7. PEP8
-# Для устойчивой работы, таймаут запроса - 3 сек
+# pip install lxml
+# request limit 3 sec
 
-# Импорты
+# Imports
 import requests
 from bs4 import BeautifulSoup as Bs
 import re
 
-# Ресурсы
+# Resources
 url = 'https://tass.ru'
 
 
 def tass_parser_refs():
-    """Возвращает список адресов новостных страниц (ref's)"""
-    
+    """Returns a list of news page addresses (ref's)"""
+
     source = requests.get(url).text
     soup = Bs(source, 'lxml').find('main', class_='container')
     refs = []
     raw_result = soup.find_all_next('a', href=True)
-    
+
     for ref in raw_result:
         if re.search('[0-9]', ref.get('href')) \
                 and 'http' not in ref.get('href') \
                 and 'press' not in ref.get('href'):
             refs.append(ref.get('href'))
-    
+
     return refs
 
 
 def tass_parser_body(ref):
-    """Возвращает 3 тыс. символов текста новости в ответ на ее адрес
-    https://tass.ru/ref (например, '/politika/7999199')"""
-    
+    """Returns 3 thousand characters of the text of the news in response
+    to its address https://tass.ru/ref (example, '/politika/7999199')"""
+
     source = requests.get(f'{url}{ref}').text
     soup = Bs(source, 'lxml')
     raw_result = soup.find_all('div', class_='text-content')
     result = raw_result[0].text[:3000]
-    
+
     return result
