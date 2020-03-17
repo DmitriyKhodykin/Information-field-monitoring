740487c2345c3397344cae372f435d680285938d
Dmitriy <49707091+DmitriyKhodykin@users.noreply.github.com>
2020-03-17 17:02:41 +0300

    Update tass_parser.py

--- tass_parser.py
+++ tass_parser.py
@@ -12,23 +12,29 @@ url = 'https://tass.ru'
 
 def tass_parser_refs():
     """Возвращает список адресов новостных страниц (ref's)"""
+    
     source = requests.get(url).text
     soup = Bs(source, 'lxml').find('main', class_='container')
     refs = []
     raw_result = soup.find_all_next('a', href=True)
+    
     for ref in raw_result:
         if re.search('[0-9]', ref.get('href')) \
                 and 'http' not in ref.get('href') \
                 and 'press' not in ref.get('href'):
             refs.append(ref.get('href'))
+    
     return refs
 
 
 def tass_parser_body(ref):
     """Возвращает 3 тыс. символов текста новости в ответ на ее адрес
+    
     https://tass.ru/ref (например, '/politika/7999199')"""
+    
     source = requests.get(f'{url}{ref}').text
     soup = Bs(source, 'lxml')
     raw_result = soup.find_all('div', class_='text-content')
     result = raw_result[0].text[:3000]
+    
     return result
