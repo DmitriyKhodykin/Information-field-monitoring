f4d70f6765268599dc9091c586c55fa1d1e54edf
Dmitriy <49707091+DmitriyKhodykin@users.noreply.github.com>
2020-03-18 22:16:03 +0300

    Update interfax.py

--- interfax.py
+++ interfax.py
@@ -1,42 +1,31 @@
-# Модуль для разбора новостной ленты "Интерфакс"
+# Module for uploading INTERFAX news feeds to a local database
+# Python 3.7. PEP8
 # pip install lxml
 
-# Импорты
+# Imports
 import requests
-from bs4 import BeautifulSoup
-import pandas as pd
+from bs4 import BeautifulSoup as Bs
+import re
+
+# Resources
+url = 'https://www.interfax.ru'
 
 
-def interfax():
-    """Возвращает заголовки новостей с главной страницы Интерфакс"""
-    source = requests.get('https://www.interfax.ru/russia/').text
-    soup = BeautifulSoup(source, 'lxml')
+def interfax_parser_refs():
+    """Returns a list of news page addresses (ref's)"""
 
-    news_list = []
-    
-    # Блок главный новостей
-    for main_line in soup.find_all('div', class_='newsmain'):
-        news_main = main_line.h3.text.split('/')
-        for item in news_main:
-            news_list.append(item)
-    
-    # Блок новостной ленты
-    for time_line in soup.find_all('div', class_='timeline__group'):
-        news_line = time_line.h3.text.split('/')
-        for item in news_line:
-            news_list.append(item)
+    source = requests.get(url).text
+    soup = Bs(source, 'lxml').find('main')
+    refs = []
+    raw_result = soup.find_all_next('a', href=True)
 
-    # Блок главных новостей под фото
-    for photo_line in soup.find_all('div', class_='timeline__photo'):
-        photo_news = photo_line.h3.text.split('/')
-        for item in photo_news:
-            news_list.append(item)
+    for ref in raw_result:
+        if re.search('[0-9]', ref.get('href')) \
+                and 'http' not in ref.get('href') \
+                and 'photo' not in ref.get('href') \
+                and 'html' not in ref.get('href') \
+                and 'story' not in ref.get('href') \
+                and 'pressreleases' not in ref.get('href'):
+            refs.append(ref.get('href'))
 
-    # Еще один блок ленты новостей
-    for text_line in soup.find_all('div', class_='timeline__text'):
-        text_news = text_line.h3.text.split('/')
-        for item in text_news:
-            news_list.append(item)
-
-    df_interfax = pd.DataFrame(news_list)
-    return df_interfax
+    return refs
