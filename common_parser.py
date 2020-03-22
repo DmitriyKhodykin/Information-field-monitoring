1c4c610600be23f7fc97bc5638cfea963ff9fdb5
Dmitriy <49707091+DmitriyKhodykin@users.noreply.github.com>
2020-03-22 21:59:42 +0300

    Update common_parser.py

--- common_parser.py
+++ common_parser.py
@@ -6,68 +6,111 @@
 import requests
 from bs4 import BeautifulSoup as Bs
 import re
+import time
+
 
 class Parser:
 
-
     def __init__(self, url):
         self.url = url
-    
 
     def get_parser_refs(self):
         """Returns a list of news page addresses"""
 
         source = requests.get(self.url).text
 
-        #Create soup object
-        if("ria" in self.url):
+        # Create soup object
+        if "ria" in self.url:
             soup = Bs(source, 'lxml').find('span', class_='share')
-        elif("tass" in self.url):
+        elif "tass" in self.url:
             soup = Bs(source, 'lxml').find('main', class_='container')
-        elif("interfax" in self.url):
+        elif "interfax" in self.url:
             soup = Bs(source, 'lxml').find('main')
         else:
-            pass   
+            pass
 
         refs = []
         raw_result = soup.find_all_next('a', href=True)
 
-        #Get List of all refs
-        if("ria" in self.url):
+        # Get List of all refs
+        if "ria" in self.url:
             for ref in raw_result:
-                if all(elm in ref.get('href') for elm in ['html','http']):
+                if all(elm in ref.get('href') for elm in ['html', 'http']):
                     refs.append(ref.get('href'))
 
-        elif("tass" in self.url):
+        elif "tass" in self.url:
             for ref in raw_result:
-                if re.search('[0-9]', ref.get('href')) and all(elm not in ref.get('href') for elm in ['http','press']):
+                if re.search('[0-9]', ref.get('href')) and all(elm not in ref.get('href') for elm in ['http', 'press']):
                     refs.append(ref.get('href'))
-            
-        elif("interfax" in self.url):
+
+        elif "interfax" in self.url:
             for ref in raw_result:
-                if re.search('[0-9]', ref.get('href')) and  all(elm not in ref.get('href') for elm in ['http','photo','html','story','pressreleases']):
-                 refs.append(ref.get('href'))
+                if re.search('[0-9]', ref.get('href')) and all(
+                        elm not in ref.get('href') for elm in [
+                            'http', 'photo', 'html', 'story', 'pressreleases', 'asp', 'aeroflot'
+                        ]):
+                    refs.append(ref.get('href'))
 
         else:
             pass
 
         return refs
 
+    def get_parser_body(self, ref):
+        """Returns 4 thousand characters of the text of the news in response"""
 
-    def get_parser_body(self,ref):
-        """Returns 3 thousand characters of the text of the news in response
-            to its address https://tass.ru/ref (example, '/politika/7999199')"""
+        result = str()
 
-        source = requests.get(f'{self.url}{ref}').text
-        soup = Bs(source, 'lxml')
-        raw_result = soup.find_all('div', class_='text-content')
-        result = raw_result[0].text[:3000]
+        if "ria" in self.url:
+            source = requests.get(ref).text
+            soup = Bs(source, 'lxml')
+            raw_result = soup.find_all('div', class_='article__text')
 
-        return result
+            for i in raw_result:
+                result = result + i.text + ' '
+
+        elif "interfax" in self.url:
+            source = requests.get(f'{self.url}{ref}')
+            source.encoding = 'cp1251'
+            source = source.text
+            soup = Bs(source, 'lxml').find('div', class_='mainblock')
+            raw_result = soup.find_all_next('p')
+
+            for i in raw_result:
+                result = result + i.text + ' '
+
+        elif "tass" in self.url:
+            source = requests.get(f'{self.url}{ref}').text
+            soup = Bs(source, 'lxml')
+            raw_result = soup.find_all('div', class_='text-content')
+            result = result + raw_result[0].text
+
+        else:
+            pass
+
+        return result[:4000]
 
 
+if __name__ == "__main__":
 
+    def test():
+        urls = [
+            'https://www.ria.ru',
+            'https://www.interfax.ru',
+            'https://tass.ru'
+                ]
+        nn = 0
 
+        for url in urls:
+            parser = Parser(url)
+            refs = parser.get_parser_refs()
+            for ref in refs:
+                print(
+                    nn,
+                    ref,
+                    parser.get_parser_body(ref)
+                )
+                time.sleep(1)
+                nn = nn + 1
 
-
-
+    test()
