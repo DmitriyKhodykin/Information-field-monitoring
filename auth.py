bedb2fd0efc8cd9bd443965c384ca1e7ead60362
Dmitriy <49707091+DmitriyKhodykin@users.noreply.github.com>
2020-03-30 17:33:07 +0300

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,12 @@
+# Autorizations data
+
+host_ifm = '*****'
+db_ifm = 'information_field'
+user_ifm = '*****'
+passwd_ifm = '*****'
+
+urls = [
+    'https://www.ria.ru',
+    'https://www.interfax.ru',
+    'https://tass.ru'
+]
